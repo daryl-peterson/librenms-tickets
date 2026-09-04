c15bb6778dd8cea1cdc3925557df5d896fe32fd1
daryl-peterson <daryl.peterson@gmail.com>
2026-09-04 10:54:52 -0500

    Updating

---
+++ src/Controllers/ActionController.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Librenms Tickets Action Controller
+ *
+ * @package     librenms-tickets
+ * @author      Daryl Peterson <@gmail.com>
+ * @license     https://opensource.org MIT License
+ * @link        https://github.com/daryl-peterson/
+ * @since       1.0.0
+ */
+
+namespace DRP\Tickets\Controllers;
+
+
+
+/**
+ * Librenms Tickets Action Controller
+ *
+ * @package     librenms-tickets
+ * @author      Daryl Peterson <@gmail.com>
+ * @copyright   Copyright (c) 2026, Daryl Peterson
+ * @license     https://opensource.org MIT License
+ * @link        https://github.com/daryl-peterson/
+ * @since       1.0.0
+ */
+class ActionController {
+    public function __construct() {
+        # Code Here
+    }
+}
