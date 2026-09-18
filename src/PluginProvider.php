09e733726108429d8c1b3036cdeb318c66a8a87b
daryl-peterson <daryl.peterson@gmail.com>
2026-09-18 10:31:58 -0500

    Updating

--- src/PluginProvider.php
+++ src/PluginProvider.php
@@ -16,21 +16,15 @@ namespace DRP\Tickets;
 use LibreNMS\Plugins;
 use App\Plugins\PluginManager;
 use LibreNMS\Interfaces\Plugins\PluginManagerInterface;
-use LibreNMS\Interfaces\Plugins\Hooks\DeviceOverviewHook as DeviceOverviewHookInterface;
 use LibreNMS\Interfaces\Plugins\Hooks\MenuEntryHook as MenuEntryHookInterface;
 use LibreNMS\Interfaces\Plugins\Hooks\SettingsHook as SettingsHookInterface;
 use LibreNMS\Interfaces\Plugins\Hooks\SinglePageHook;
-
-
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\View;
 
-use DRP\Tickets\Hooks\DeviceOverview;
 use DRP\Tickets\Hooks\Menu;
 use DRP\Tickets\Hooks\Page;
 use DRP\Tickets\Hooks\Settings;
-use DRP\Tickets\PluginSettings;
 
 
 /**
@@ -43,7 +37,7 @@ use DRP\Tickets\PluginSettings;
  * @link        https://github.com/daryl-peterson/
  * @since       0.0.1
  */
-class TicketServiceProvider extends ServiceProvider {
+class PluginProvider extends ServiceProvider {
 
     public function register(): void {
         # code
@@ -51,15 +45,6 @@ class TicketServiceProvider extends ServiceProvider {
 
     public function boot(): void {
         $pluginName = 'librenms-tickets';
-
-        $hasRedis = checkRedis();
-        if (! $hasRedis) {
-            $obj = new PluginSettings();
-            $obj->set('redis', false);
-        } else {
-            config(['queue.default' => 'redis']);
-        }
-
 
         /*
          * Compatibility view path.
@@ -70,34 +55,19 @@ class TicketServiceProvider extends ServiceProvider {
          * Package views can also be referenced as:
          * device-importer::page
          */
-
-        $rootPath = base_path();
-        $viewPath = $rootPath . '/vendor/daryl-peterson/librenms-tickets/resources/views';
         $paths = [
             __DIR__ . '/..',
             __DIR__ . '/../resources/views',
-            $viewPath,
-
         ];
         Log::debug('View paths: ' . PHP_EOL . print_r($paths, true));
-
-
         $this->loadViewsFrom($paths, 'librenms-tickets');
-        //$this->loadViewsFrom(__DIR__ . '/../resources/views', 'librenms-tickets');
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-
-        $paths = View::getFinder()->getPaths();
-        Log::debug('Current view paths: ' . PHP_EOL . print_r($paths, true));
-
+        //$this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         /**
          * @var PluginManager $pluginManager
          */
         $pluginManager = $this->app->make(PluginManagerInterface::class);
-
-
-        //$pluginManager->publishHook($pluginName, DeviceOverviewHookInterface::class, DeviceOverview::class);
         $pluginManager->publishHook($pluginName, MenuEntryHookInterface::class, Menu::class);
         $pluginManager->publishHook($pluginName, SinglePageHook::class, Page::class);
         $pluginManager->publishHook($pluginName, SettingsHookInterface::class, Settings::class);
