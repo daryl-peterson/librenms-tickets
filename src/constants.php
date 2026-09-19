c4303ca9ff73733518a6b3d57ea8624d4f8bd102
daryl-peterson <daryl.peterson@gmail.com>
2026-09-18 22:20:11 -0500

    Updating

--- src/constants.php
+++ src/constants.php
@@ -2,15 +2,36 @@
 
 namespace DRP\Tickets;
 
+/**
+ * Plugin information constants.
+ */
 const PLUGIN_NAME          = 'librenms-tickets';
 const PLUGIN_TITLE         = 'LibreNMS Tickets';
 const PLUGIN_AUTHOR        = 'Daryl Peterson';
 const PLUGIN_VER           = 'v0.1.0-alpha.01';
 
-
+/**
+ * Database connection constants.
+ */
 const PLUGIN_DB_HOST        = '127.0.0.1';
 const PLUGIN_DB_PORT        = '3306';
 const PLUGIN_DB_CONNECTION  = 'plugin_db';
 const PLUGIN_DB_DATABASE    = 'librenms_plugin_db';
 const PLUGIN_DB_USERNAME    = 'plugin_user';
 const PLUGIN_DB_PASSWORD    = 'plugin_password';
+
+
+/**
+ * Database cache keys for connection checks.
+ */
+const CACHE_DB_PASS = 'db_pass';
+
+/**
+ * Database cache key for migration checks.
+ */
+const CACHE_DB_MIGRATION_CHECK = 'db_migration_check';
+
+/**
+ * @var string The cache key for database errors.
+ */
+const CACHE_DB_ERROR = 'db_error';
